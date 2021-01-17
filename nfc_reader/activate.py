005f1d6d9135dfe2c2d49836634e05644883b544
laygond <laygond_bryan@hotmail.com>
2021-01-17 18:14:06 -0500

    :sparkles: (des)activate card functionality

---
+++ nfc_reader/activate.py
@@ -0,0 +1,69 @@
+# -----------------------------
+#   USAGE
+# -----------------------------
+# python activate.py --input 'P5O4' --switch 'ON'
+#
+# DESCRIPTION:
+# This script is run in response to a change of state in any of the ON/OFF switches from the
+# Openhab sitemap. For the "groupData.csv" file, sets to True or Flase the 'Active' field of 
+# the row who lives in the given input floor&office, e.g., P5O4. If this row owns 
+# several floor&offices then prints the remaining floor&offices so that OpenHab can turn
+# ON/OFF the remaining switches. 
+#
+# Author: Bryan Laygond
+# Website: http://www.laygond.com
+#
+# COPYRIGHT:
+# Code may only be distributed through http://www.laygond.com any other methods
+# of obtaining or distributing are prohibited
+# Copyright (c) 2020-2021
+# ------------------------------
+
+# Import the necessary packages
+import csv
+import numpy as np
+import pandas as pd
+import time
+import argparse
+
+# Construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", type=str, default="",
+	help=" Floor&Office of Group owner")
+ap.add_argument("-s", "--switch", type=str, default="",
+	help=" Switch State")
+args = vars(ap.parse_args())
+
+# Read csv and find row of Floor&Office
+df = pd.read_csv('groupData.csv')
+index_row = df.index[df['PisoOficina'].str.contains(args["input"], na=False)]
+
+# Toggle Active Status
+if args["switch"] == 'ON':
+    df.loc[index_row,'Active'] = True
+if args["switch"] == 'OFF':
+    df.loc[index_row,'Active'] = False
+
+# Print if Group owner has more floor&offices
+floorOffices = df.loc[index_row,'PisoOficina'].values[0]
+if len(floorOffices) > 4: # then it has more 
+    floorOffices = floorOffices.split(',')
+    floorOffices.remove(args["input"])
+    print(",".join([elem for elem in floorOffices]))
+
+# Update csv file 
+df.to_csv('groupData.csv', index=False)
+
+# TODO:
+# Reading as list and then converting seems faster
+# * try  a pure a list solution
+# * try csv.DictReader too
+# start  = time.time()
+# with open('groupData.csv','r') as dest_f:
+#     data_iter = csv.reader(dest_f,
+#                            delimiter = ',',
+#                            quotechar = '"')
+#     data = [data for data in data_iter]
+#     df = pd.DataFrame(data)
+#     print(df)
+# print("[INFO] set took:", time.time()-start) 
--- nfc_reader/read.py
+++ nfc_reader/read.py
@@ -1,8 +1,18 @@
 import csv
 import nfc
 import ndef
+import threading
+import binascii
+import time
 
 # Tag: A card or phone is considered an NFC tag 
+
+# NFC Reader must be given access rights before running code as mentioned:
+# https://nfcpy.readthedocs.io/en/latest/topics/get-started.html#installation
+
+# Hardware:
+# - Sony RC-S380            https://www.sony.net/Products/felica/business/tech-support/
+# - Card NTAG215 type 2     https://www.nxp.com/docs/en/data-sheet/NTAG213_215_216.pdf
 
 # def checkTag(tag_ID):
 #     """
@@ -24,38 +34,53 @@ import ndef
 #                         #Welcome row["User"], led green, activate button panel
 #                     else:
 #                         #led red
-
-# NFC Reader must be given access rights before running code as mentioned:
-# https://nfcpy.readthedocs.io/en/latest/topics/get-started.html#installation
+# Website that will be pushed to phone when tapped
+phone_website = "https://www.instagram.com/laygond/"
 
 # Verify Reader is available
 clf = nfc.ContactlessFrontend()
 sony_RCS380 = 'usb:054c:06c1'       # Product and Vendor ID
-print(clf.open(sony_RCS380))
+print("[INFO] Usb NFC Reader Connected: ", clf.open(sony_RCS380))
 
-# Check tag's type of communication(target) and ID 
-from nfc.clf import RemoteTarget
-target = clf.sense(RemoteTarget('106A'), RemoteTarget('106B'), RemoteTarget('212F'))
-print(target)
+# # Sense and activate NFC tag instantaneously (fails if tag is not on reader) 
+# from nfc.clf import RemoteTarget
+# target = clf.sense(RemoteTarget('106A'), RemoteTarget('106B'), RemoteTarget('212F'))
+# print(target)
 # tag = nfc.tag.activate(clf, target)
 # print(tag)
 
-# Check tag ID (preferred method)
-tag = clf.connect(rdwr={'on-connect': lambda tag: False})
-print(tag)
+while True:
+    try:
+        # Connect: Sense and activate NFC tag when tapped (preferred method)
+        # More Parameters here: https://nfcpy.readthedocs.io/en/latest/modules/tag.html#nfc.tag.Tag
+        tag = clf.connect(rdwr={'on-connect': lambda tag: False})
+        print("[INFO] General:", tag)
+        print("[INFO] UID:", binascii.hexlify(tag.identifier).decode())
 
-ho = clf.connect(llcp={}) # tap with phone
-print(ho)
+        # Callback functions to push website if NFC tag is a phone
+        def send_ndef_message(llc):
+            # this stage is only reached after a successful llc connection
+            snep = nfc.snep.SnepClient(llc)
+            snep.put_records([ndef.UriRecord(phone_website)])
+            
+        def immediately():
+            global started
+            return time.time() - started > .5
 
-# # Write to tag
-# uri, title = 'http://nfcpy.org', 'nfcpy project'
-# tag.ndef.records = [ndef.SmartposterRecord(uri, title)]
+        def llcp_connected(llc):
+            threading.Thread(target=send_ndef_message, args=(llc,)).start()
+            return True    # False returns llc object to be handled
+                        # True otherwise
 
-# Read from tag
-for record in tag.ndef.records:
-    print(record)
+        # Push website to phone otherwise end attempt
+        global started
+        started = time.time()
+        clf.connect(llcp={'on-connect':llcp_connected, 'lto':250, 'role':'initiator'}, terminate=immediately) 
+    
+    except KeyboardInterrupt:
+        break
+    except Exception as e:
+        print(e)
+        pass
+    
 
-
-# record = tag.ndef.records[0]
-# print(record.type)
-# print(record.uri)
