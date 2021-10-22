3a5fd070df5d5db11965034d15f9956ff019476d
laygond <laygond_bryan@hotmail.com>
2021-10-22 18:47:03 -0400

    :sparkles: faster check_tag

--- nfc_reader/check_tag_deprecated.py
+++ nfc_reader/check_tag_deprecated.py
@@ -1,14 +1,13 @@
 # -----------------------------
-#   USAGE
+#   USAGE (DEPRECATED: Left for educational purposes)
 # -----------------------------
-# python check_tag.py 
+# python check_tag_deprecated.py 
 #
 # DESCRIPTION:
 # Reads continously UUID from NFC tags: cards, phones, etc
 # Checks if a tag is registered in the group database and active.
-# If tag is active then set as true the 'enable' pickle file at 'Elevator-AI/botonera'.
-# which indicates temporal access to the elevator's button panel. 
-# An LED strip for visual notification is turned on (green=active or red=non-active)
+# If tag is active then enable through a relay the elevator button pannel.
+# Mqtt messages are sent to an LED strip for visual notification (green or red)
 # Also, any tag UUID is sent as an MQTT message to openhab's server for record.
 # Also, if tag is a phone then push NDEF data to a website. 
 #
@@ -40,29 +39,22 @@ import time
 import os
 import paho.mqtt.client as mqtt 
 import RPi.GPIO as GPIO
-import pickle
 
-# LED Strip Setup for visual notification
-GPIO.setmode(GPIO.BCM)             # GPIO Numbers instead of board pin numbers
-LED_R_GPIO = 2                     # Red pin
-LED_G_GPIO = 3                     # Green pin
-LED_B_GPIO = 4                     # Blue pin
-GPIO.setup(LED_R_GPIO, GPIO.OUT)   # Set pin as output
-GPIO.setup(LED_G_GPIO, GPIO.OUT)    
-GPIO.setup(LED_B_GPIO, GPIO.OUT)    
-r = GPIO.PWM(LED_R_GPIO, 100)      # PWM from 0-100
-g = GPIO.PWM(LED_G_GPIO, 100)
-b = GPIO.PWM(LED_B_GPIO, 100)
-r.start(0)
-g.start(0)
-b.start(0)
+# Relay Setup to enable elevator button panel
+relay_started = time.time()         # keep track of time when relay on
+GPIO.setmode(GPIO.BCM)              # GPIO Numbers instead of board pin numbers
+RELAY_GPIO = 17
+GPIO_ON  = 0                        # Since my relay is active low
+GPIO_OFF = 1
+GPIO.setup(RELAY_GPIO, GPIO.OUT)    # Set pin as output
+GPIO.output(RELAY_GPIO, GPIO_OFF)   # Start with relay off
 
 # Website that will be pushed to phone when tapped
 phone_website = "https://condominioimbabura.github.io/"
 
 # MQTT Setup
 mqttBroker ="192.168.0.205" 
-client = mqtt.Client("PyMQ")
+client = mqtt.Client("PythonMQTT")
 client.connect(mqttBroker)
 
 # Verify Reader is available
@@ -85,31 +77,9 @@ def llcp_connected(llc):
     return True     # False returns llc object to be handled
                     # True otherwise
 
-
-# Callback to turn on and off LED Strip
-def led_signal(channel):
-    channel.ChangeDutyCycle(100)
-    time.sleep(.8)
-    channel.ChangeDutyCycle(0)
-
-
-# Set csv path point to active group and pickle path to enable 
+# Set csv path point to active group
 dir_path = os.path.dirname(os.path.realpath(__file__))
 csv_path = os.path.join(dir_path,'groupData.csv')
-pickle_path = os.path.join(dir_path,'../botonera/enable')
-
-# Callback to Disable Button Pannel after 5 seconds 
-# (Set enable pickle file to false)
-def disable_button_panel(time_since_enable_started):
-    while(time.time() - time_since_enable_started < 5): pass      # hold time for t seconds
-    print(time.strftime("%H %M %S",time.localtime()))
-    # p = open(pickle_path,'rwb')
-    # botonera_enabled_through_tag = pickle.load(p)
-    # if botonera_enabled_through_tag:
-    #     botonera_enabled_through_tag = False
-    #     pickle.dump(botonera_enabled_through_tag,p)
-    # p.close()
-
 
 # Read NFC tag when tapped (preferred method) (first instances of variables)
 tag  = clf.connect(rdwr={'on-connect': lambda tag: False})
@@ -133,28 +103,17 @@ while True:
             if uuid in row['UUID']:
                 #print(row)
                 if row["Active"] == 'True':
-                    print("[INFO]=== TAG IS ACTIVE ===")                    
-                    # Enable Button Panel
-                    p = open(pickle_path,'wb')
-                    botonera_enabled_through_tag= True
-                    pickle.dump(botonera_enabled_through_tag, p)
-                    p.close()
-
-                    # Track time and disable Button Pannel after 5 seconds
-                    threading.Thread(target=disable_button_panel, args=(time.time(),)).start()
-
-                    # GREEN light
-                    threading.Thread(target=led_signal, args=[g]).start() 
-                    
-                    # Reset file pointer
+                    #print("[INFO] TRUE BABY")
+                    client.reconnect() # in case broker disconnects
+                    client.publish("elevator/control/led", "0;100;0") #GREEN light 
+                    GPIO.output(RELAY_GPIO, GPIO_ON) #Enable Button Panel and track time
+                    relay_started = time.time()
                     f.seek(0)
                     break
                 else:
-                    print("[INFO]=== PAY YOUR BILLS BABY ===")    
-                    # RED light
-                    threading.Thread(target=led_signal, args=(r,)).start()
-
-                    # Reset file pointer
+                    #print("[INFO] FALSE BABY") 
+                    client.reconnect() # in case broker disconnects   
+                    client.publish("elevator/control/led", "100;0;0") #RED light
                     f.seek(0)
                     break
         
@@ -164,7 +123,11 @@ while True:
         #print("about to push")
         clf.connect(llcp={'on-connect':llcp_connected, 'lto':250, 'role':'initiator'}, terminate=immediately) 
         #print("done pushing")
-  
+
+        # Disable Button Pannel after 3 seconds (Set Relay off)
+        while(time.time() - relay_started < 3): pass      # hold time for t seconds
+        GPIO.output(RELAY_GPIO, GPIO_OFF) 
+
     except:
         pass
 
